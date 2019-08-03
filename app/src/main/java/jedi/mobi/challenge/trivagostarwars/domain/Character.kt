6aa818528b3754600c8997451ee732a5e770f23a
Alexey Kuznetsov <alkuznetsov@ozon.travel>
2019-08-03 17:38:46 +0300

    add character entity

---
+++ app/src/main/java/jedi/mobi/challenge/trivagostarwars/domain/Character.kt
@@ -0,0 +1,28 @@
+package jedi.mobi.challenge.trivagostarwars.domain
+
+import java.util.*
+
+data class Character(
+    val name: String,
+    val birthYear: String,
+    val heightCm: Float,
+    val heightInch: Float,
+    val heightFeet: Float,
+    val species: List<Species>,
+    val planet: Planet,
+    val films: List<Film>
+)
+
+data class Species(
+    val name: String,
+    val language: String,
+    val homeWorld: String
+)
+
+data class Planet(val population: Long)
+
+data class Film(
+    val title: String,
+    val releaseDate: Date,
+    val openingCrawl: String
+)
